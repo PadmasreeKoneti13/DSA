20ee2ff54a7b6925853fb57966c77ee5ca8415de
PadmasreeKoneti13 <padmasreekoneti1311@gmail.com>
2026-08-04 19:55:36 +0530

    Time: 3 ms (79.93%) | Memory: 20.6 MB (7.75%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -7,8 +7,7 @@ class Solution:
             curr_sum = numbers[left] + numbers[right]
             if curr_sum == target:
                 return [left+1,right+1]
+            elif curr_sum < target:
+                left += 1
             else:
-                if curr_sum < target:
-                    left += 1
-                else:
-                    right -= 1
+                right -= 1
